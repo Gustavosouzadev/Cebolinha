753b2d1f8db025862bcb53b4cad262d642d6f54c
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-13 17:44:32 -0300

    Finalizando Cebolinha

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+
+            string texto = "Escreva alguma frase para"; 
+            string texto2 = "o Cebolinha reproduzir";
+            string padrao;
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("_______________________________");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"+| {texto} |+");
+            Console.WriteLine($"   | {texto2} |");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("|_____________________________|");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Sua Frase: ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            padrao = Console.ReadLine();
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("-Cebolinha: ");
+
+            Console.ResetColor();
+            Console.Write($"{padrao.Replace("R", "L").Replace("r", "l")}");
+
+            Console.Beep();
+        }
+    }
+}
